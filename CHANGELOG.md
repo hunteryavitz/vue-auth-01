1d1f611e2e1b2c61d0d99b67ac48122c4366195b
hunteryavitz <hunteryavitz@protonmail.com>
2022-06-26 15:33:07 -0700

    initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,16 @@
+# CHANGELOG
+
+## Vue Auth POC App
+
+---
+### Unreleased
+
+### v0.0.0.1 - 06/26/2022
+
+---
+## ADDED
+- dependencies for [jest](https://jestjs.io/docs/getting-started)
+- dependencies for [vue router](https://router.vuejs.org/)
+- dependencies for [firebase](https://console.firebase.google.com/project/vue-auth-01-9673f/overview)
+- src/firebase/index.js file to configure [firebase](https://console.firebase.google.com/project/vue-auth-01-9673f/overview)
+- changelog
---
+++ src/firebase/index.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCaZNh9F8GazuODPYI5Ag6hS_kH11w2JSw",
+    authDomain: "vue-auth-01-9673f.firebaseapp.com",
+    projectId: "vue-auth-01-9673f",
+    storageBucket: "vue-auth-01-9673f.appspot.com",
+    messagingSenderId: "954650364367",
+    appId: "1:954650364367:web:45a55d2836660d3f83eaba",
+    measurementId: "G-R0DLV11PX2"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth }
